654a478c5c4287ef730a7cf733630ff9243e2035
Bella Solo <bella.solomon-falkfrii@outlook.com>
2023-11-14 16:16:00 +0100

    added new pic for header

--- frontend/src/components/SigninPage.jsx
+++ frontend/src/components/SigninPage.jsx
@@ -1,80 +1,21 @@
 import React from "react";
-import { useFormik } from "formik";
+import Input from "../components/Forms/Input";
+// import { Button } from "@mui/material/Button";
 
 const SignInPage = () => {
-  const formik = useFormik({
-    initialValues: {
-      username: "",
-      password: "",
-    },
-    validate: (values) => {
-      const errors = {};
-
-      if (!values.username) {
-        errors.username = "Required";
-      }
-
-      if (!values.password) {
-        errors.password = "Required";
-      }
-
-      return errors;
-    },
-
-    onSubmit: async (values) => {
-      try {
-        const response = await fetch("http://localhost:3001/api/signin", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(values),
-        });
-
-        if (response.ok) {
-          const data = await response.json();
-          console.log(data.message);
-        } else {
-          const errorData = await response.json();
-          console.error(errorData.message);
-        }
-      } catch (error) {
-        console.error(error.message);
-      }
-    },
-  });
-
   // function SignInPage() {
   return (
     <>
       <div>
         <h2>Sign in</h2>
         <p>Please put your credentials in the fields</p>
-        <form onSubmit={formik.handleSubmit}>
-          <div>
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              onChange={formik.handleChange}
-              value={formik.values.username}
-            />
-          </div>
-
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              onChange={formik.handleChange}
-              value={formik.values.password}
-            />
-          </div>
-
-          <button type="submit">Login</button>
-        </form>
+        <div>
+          <Input Label={"Username"} />
+          <Input Label={"Password"} />
+          {/* <Button color="tertiary" variant="outlined">
+            Sign In
+          </Button> */}
+        </div>
       </div>
     </>
   );
