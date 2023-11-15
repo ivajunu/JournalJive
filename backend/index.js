4bcbd9710742513eef6b8f70473143609f570b8c
Bella Solo <bella.solomon-falkfrii@outlook.com>
2023-11-15 14:20:44 +0100

    edit in backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+path = require("path");
 const cors = require("cors");
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 app.use(
@@ -10,6 +12,17 @@ app.use(
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
   })
 );
+
+const client = new Client({
+  connectionString: process.env.PGURI,
+});
+
+client.connect();
+
+app.get("/api", async (request, response) => {
+  const { rows } = await client.query("SELECT * FROM blogpost_person");
+  response.send(rows);
+});
 
 //Nedan är kod för att kunna logga in -Bella
 app.post("/api/signin", async (req, res) => {
@@ -20,6 +33,8 @@ app.post("/api/signin", async (req, res) => {
     res.status(401).send("Invalid credentials");
   }
 });
+
+app.use(express.static(path.join(path.resolve(), "dist")));
 
 app.listen(port, () => {
   console.log(`Välkommen till port ${port}`);
