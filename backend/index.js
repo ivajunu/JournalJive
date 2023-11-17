75c023699cdb09b2953333e1063963215b5392ac
Bella Solo <bella.solomon-falkfrii@outlook.com>
2023-11-17 12:06:37 +0100

    made it possible to sign in

--- backend/index.js
+++ backend/index.js
@@ -17,18 +17,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const signinpage = require("./routes/signinpage");
-app.use(signinpage);
+const signin = require("./routes/signin");
+signin.client = client;
+app.use(signin);
 
 const createaccount = require("./routes/createaccount");
 createaccount.client = client;
 app.use(createaccount);
 
+// hämtar alla bloggar
 app.get("/api", async (_request, response) => {
   const { rows } = await client.query("SELECT * FROM blogs");
   response.send(rows);
 });
 
+// hämtar alla användaare
 app.get("/users", async (_request, response) => {
   const { rows } = await client.query("SELECT * FROM person_datas");
   console.log("Hämta användare");
--- backend/routes/signin.js
+++ backend/routes/signin.js
@@ -3,27 +3,14 @@ const router = express.Router();
 // const app = express();
 const client = require("../index");
 
-router.get("/signin", async (_req, res) => {
-  try {
-    const { rows } = await client.query("SELECT * FROM person_datas");
-    console.log("Hämtar alla användare");
-    res.send(rows);
-  } catch (error) {
-    console.error("Error fetching users", error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
-
 router.post("/signin", async (req, res) => {
   try {
-    const person_username = req.body.person_username;
-    const person_password = req.body.person_password;
+    const { username, password } = req.body;
 
-    const result = await client.query(
+    const result = await router.client.query(
       "SELECT * FROM person_datas WHERE person_username = $1 AND person_password = $2",
-      [person_username, person_password]
+      [username, password]
     );
-
     if (result.rows.length > 0) {
       res.status(200).json({ message: "Successfully signed in" });
     } else {
@@ -35,4 +22,19 @@ router.post("/signin", async (req, res) => {
   }
 });
 
+router.get("/signin", async (_req, res) => {
+  try {
+    const { rows } = await router.client.query(
+      "SELECT person_username, person_password FROM person_datas"
+    );
+    console.log("Hämtar inloggningsuppgifter");
+    res.send(rows);
+  } catch (error) {
+    console.error("Error fetching users", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 module.exports = router;
+
+// gör en get sök igenom reslutatet match by i databas
