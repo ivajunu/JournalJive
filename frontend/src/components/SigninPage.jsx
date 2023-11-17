4b2a1ed0c9a15e8d82b01282524ba8059ec2ede5
Bella Solo <bella.solomon-falkfrii@outlook.com>
2023-11-17 12:07:33 +0100

    sign in made possible

--- frontend/src/components/SigninPage.jsx
+++ frontend/src/components/SigninPage.jsx
@@ -1,9 +1,12 @@
-// import React from "react";
+import { useState } from "react";
 // import CustomButton from "./Forms/Button";
 import Input from "../components/Forms/Input";
 import FormButton from "./Forms/FormButton";
 
 const SignInPage = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
   const textStyle = {
     margin: "2rem",
   };
@@ -14,39 +17,70 @@ const SignInPage = () => {
   };
   const buttonStyle = { margin: "1rem" };
 
+  // function handleClick(e) {
+  //   e.preventDefault();
+  //   const signinValue = {
+  //     username: userName,
+  //     password: password,
+  //   };
+  //   console.log(signinValue);
+  // }
+
+  async function handleClick(e) {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3000/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: userName,
+          password: password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.status === 200) {
+        alert(data.message);
+      } else {
+        alert(data.message);
+      }
+    } catch (error) {
+      console.error("Error during sign-in", error);
+    }
+  }
+
   return (
     <>
       <div style={textStyle}>
         <p>Please put your credentials in the fields</p>
       </div>
-      {/* <div style={containerStyle}>
-        <Input placeholder={"Username"} />
-        <Input type={"password"} placeholder={"Password"} />
+      <div style={containerStyle}>
+        <Input
+          placeholder={"Username"}
+          value={userName}
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+        />
+        <Input
+          type={"password"}
+          placeholder={"Password"}
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
         <div style={buttonStyle}>
           <FormButton
             label="Sign In"
             variant="outlined"
-            // onClick={handleClick}
+            onClick={handleClick}
           />
-        </div> */}
-      {/* </div> */}
-      <div style={containerStyle}>
-        <form action="http://localhost:3000/signinpage" method="post">
-          <label>
-            E-postadress
-            <input name="username" placeholder="username" value="" />
-          </label>
-          <label>
-            Lösenord
-            <input
-              name="password"
-              placeholder="password"
-              type="password"
-              value=""
-            />
-          </label>
-          <input type="submit" value="Skicka" />
-        </form>
+        </div>
       </div>
     </>
   );
