8f944be3c73b4d8b28802e0e1476e08a526c22db
Thitisuda Mahasrithanet <thiti.m90@gmail.com>
2023-11-24 10:53:04 +0100

    Fixed delete btn after feedback

--- frontend/src/components/Forms/Button.jsx
+++ frontend/src/components/Forms/Button.jsx
@@ -1,13 +1,31 @@
 import MuiButton from "@mui/material/Button";
 import PropTypes from "prop-types";
+import deleteIkon from "../../assets/trashIkon.svg";
 
 function CustomButton(props) {
-  const { label, color, variant, onClick } = props;
+  const { label, variant, onClick } = props;
 
   return (
     <div>
-      <MuiButton color={color} variant={variant} onClick={onClick}>
+      <MuiButton
+        sx={{
+          width: "10px",
+          height: "35px",
+          backgroundColor: "#ba1a08",
+          color: "#ffffff",
+          ":hover": {
+            backgroundColor: "#ee422fe5",
+          },
+        }}
+        variant={variant}
+        onClick={onClick}
+      >
         {label}
+        <img
+          style={{ width: "20px", height: "20px", marginLeft: "3px" }}
+          src={deleteIkon}
+          alt="delete icon"
+        ></img>
       </MuiButton>
     </div>
   );
@@ -15,7 +33,6 @@ function CustomButton(props) {
 
 CustomButton.propTypes = {
   label: PropTypes.string.isRequired,
-  color: PropTypes.string,
   variant: PropTypes.string,
   onClick: PropTypes.func,
 };
